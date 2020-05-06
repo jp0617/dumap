8ae8e54a697b859df8ebc91ca8cbec1ccab7dbc7
Guarin1218 <juan.guarin8733@unaula.edu.co>
2020-05-06 10:59:29 -0500

    botones con gif

--- src/app/Components/menu/menu.component.ts
+++ src/app/Components/menu/menu.component.ts
@@ -7,16 +7,16 @@ import Swal from 'sweetalert2';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
-
+  
   constructor() {
     this.intro();
   }
-
+  
   ngOnInit(): void {
   }
-
-
-
+  
+  
+  
   glass=()=>{
     Swal.fire({
       title: '¿DESEA SABER QUÉ SE RECICLA EN ESTA SECCIÓN?',
@@ -33,7 +33,7 @@ export class MenuComponent implements OnInit {
       url("../../../assets/gif/5 (2).gif")
       left top
       no-repeat   `
-
+      
     }).then((result) =>{
       if(result.value){
         Swal.fire({
@@ -51,132 +51,165 @@ export class MenuComponent implements OnInit {
         }).then((result2) => {
           if(result2.value){
             Swal.fire({
-            title: 'GRACIAS POR TOMARTE TU TIEMPO Y CONTRIBUIR A ESTA CAUSA.',
-            icon: 'success',
-            timer:5000,
-            timerProgressBar:true,
-            backdrop: `
-            rgba(0,0,123,0.4)
-            url("../../../assets/gif/4.gif")
-            left top
-            no-repeat   `
+              title: 'GRACIAS POR TOMARTE TU TIEMPO Y CONTRIBUIR A ESTA CAUSA.',
+              icon: 'success',
+              timer:5000,
+              timerProgressBar:true,
+              backdrop: `
+              rgba(0,0,123,0.4)
+              url("../../../assets/gif/4.gif")
+              left top
+              no-repeat   `
             })
           }
         })
       }
     })
-}
-
-    carton=()=>{
-      Swal.fire({
-        title: 'Carton.',
-        width: 600,
-        padding: '3em',
-        background: 'url("../../../assets/img/background-abstract-texture-fabric.jpg")',
-        backdrop: `
-        rgba(0,0,123,0.4)
-        url("../../../assets/gif/planet.gif")
-        left top
-        no-repeat
-        `
-      })
-
-
-    }
-    organic=()=>{
-      Swal.fire({
-        title: 'Orgánicos.',
-        width: 600,
-        padding: '3em',
-        background: 'url("../../../assets/img/background-abstract-texture-fabric.jpg")',
-        backdrop: `
-        rgba(0,0,123,0.4)
-        url("../../../assets/gif/plant.gif")
-        left top
-        no-repeat
-        `
-      })
-
-
-    }
-    white=()=>{
-      Swal.fire({
-        title: 'Vidrios.',
-        width: 600,
-        padding: '3em',
-        background: 'url("../../../assets/img/background-abstract-texture-fabric.jpg")',
-        backdrop: `
-        rgba(0,0,123,0.4)
-        url("../../../assets/gif/recycle.gif")
-        left top
-        no-repeat
-        `
-      })
-
-
-    }
-    yellow=()=>{
-      Swal.fire({
-        title: 'Vidrios.',
-        width: 600,
-        padding: '3em',
-        background: 'url("../../../assets/img/background-abstract-texture-fabric.jpg")',
-        backdrop: `
-        rgba(0,0,123,0.4)
-        url("../../../assets/gif/recycle.gif")
-        left top
-        no-repeat
-        `
-      })
-
-
-    }
-    black=()=>{
-      Swal.fire({
-        title: 'Vidrios.',
-        width: 600,
-        padding: '3em',
-        background: 'url("../../../assets/img/background-abstract-texture-fabric.jpg")',
-        backdrop: `
-        rgba(0,0,123,0.4)
-        url("../../../assets/gif/recycle.gif")
-        left top
-        no-repeat
-        `
-      })
-
-
-    }
-    info=()=>{
-      Swal.fire({
-        title: '',
-        imageUrl:'url("")',
-        imageHeight: 250,
-        padding: '3em',
-        background:'url("")',
-        backdrop: `
-        url("../../../assets/gif/proceso.gif")
-        center
-        no-repeat
-        `
-
-      })
-    }
-    intro=()=>{
-      Swal.fire({
-        title: '',
-        imageUrl:'url("")',
-        imageHeight: 250,
-        padding: '3em',
-        timer:4000,
-        showConfirmButton:false,
-        timerProgressBar:true,
-        background:'url("")',
-        backdrop: `
-        url("../../../assets/gif/intro.gif")
-        center
-        no-repeat
-        `
-      })
-    }
   }
+  
+  carton=()=>{
+    Swal.fire({
+      title: '¿DESEA SABER QUÉ SE RECICLA EN ESTA SECCIÓN?',
+      width: 600,
+      confirmButtonText:'¡Sí, vamos a reciclar!',
+      showCancelButton:true,
+      cancelButtonText:'No, retroceder',
+      cancelButtonColor:'#d33',
+      confirmButtonColor:'green',
+      padding: '3em',
+      background: 'url("../../../assets/img/cafe-palido.png")',
+      backdrop: `
+      rgba(0,0,123,0.4)
+      url("../../../assets/gif/planet.gif")
+      left top
+      no-repeat   `
+    }).then((result) =>{
+      if(result.value){
+        Swal.fire({
+          title: 'AQUÍ VA LO QUE SE RECICLA',
+          confirmButtonText:'¡He terminado!',
+          showCancelButton:true,
+          cancelButtonText:'No quiero continuar',
+          cancelButtonColor:'#d33',
+          confirmButtonColor:'green',
+          backdrop: `
+          rgba(0,0,123,0.4)
+          url("../../../assets/gif/recycle.gif")
+          left top
+          no-repeat   `
+        }).then((result2) => {
+          if(result2.value){
+            Swal.fire({
+              title: 'GRACIAS POR TOMARTE TU TIEMPO Y CONTRIBUIR A ESTA CAUSA.',
+              icon: 'success',
+              timer:5000,
+              timerProgressBar:true,
+              backdrop: `
+              rgba(0,0,123,0.4)
+              url("../../../assets/gif/4.gif")
+              left top
+              no-repeat   `
+            })
+          }
+        })
+        
+      }
+    })
+  }
+  organic=()=>{
+    Swal.fire({
+      title: 'Orgánicos.',
+      width: 600,
+      padding: '3em',
+      background: 'url("../../../assets/img/background-abstract-texture-fabric.jpg")',
+      backdrop: `
+      rgba(0,0,123,0.4)
+      url("../../../assets/gif/plant.gif")
+      left top
+      no-repeat
+      `
+    })
+    
+    
+  }
+  white=()=>{
+    Swal.fire({
+      title: 'Vidrios.',
+      width: 600,
+      padding: '3em',
+      background: 'url("../../../assets/img/background-abstract-texture-fabric.jpg")',
+      backdrop: `
+      rgba(0,0,123,0.4)
+      url("../../../assets/gif/recycle.gif")
+      left top
+      no-repeat
+      `
+    })
+    
+    
+  }
+  yellow=()=>{
+    Swal.fire({
+      title: 'Vidrios.',
+      width: 600,
+      padding: '3em',
+      background: 'url("../../../assets/img/background-abstract-texture-fabric.jpg")',
+      backdrop: `
+      rgba(0,0,123,0.4)
+      url("../../../assets/gif/recycle.gif")
+      left top
+      no-repeat
+      `
+    })
+    
+    
+  }
+  black=()=>{
+    Swal.fire({
+      title: 'Vidrios.',
+      width: 600,
+      padding: '3em',
+      background: 'url("../../../assets/img/background-abstract-texture-fabric.jpg")',
+      backdrop: `
+      rgba(0,0,123,0.4)
+      url("../../../assets/gif/recycle.gif")
+      left top
+      no-repeat
+      `
+    })
+    
+    
+  }
+  info=()=>{
+    Swal.fire({
+      title: '',
+      imageUrl:'url("")',
+      imageHeight: 250,
+      padding: '3em',
+      background:'url("")',
+      backdrop: `
+      url("../../../assets/gif/proceso.gif")
+      center
+      no-repeat
+      `
+      
+    })
+  }
+  intro=()=>{
+    Swal.fire({
+      title: '',
+      imageUrl:'url("")',
+      imageHeight: 250,
+      padding: '3em',
+      timer:4000,
+      showConfirmButton:false,
+      timerProgressBar:true,
+      background:'url("")',
+      backdrop: `
+      url("../../../assets/gif/intro.gif")
+      center
+      no-repeat
+      `
+    })
+  }
+}
